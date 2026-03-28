ea5c0e9329c4abc07c21b2b3104084c924757d72
Pablo57396 <vasilevtimur59@gmail.com>
2026-03-28 19:27:07 +0200

    В2

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function showSection(id) {
-  document.getElementById("story").style.display = "none";
-  document.getElementById("characters").style.display = "none";
-  document.getElementById(id).style.display = "block";
+    let sound = document.getElementById("clickSound");
+
+    if (sound) {
+        sound.currentTime = 0;
+        sound.play();
+    }
+
+    document.getElementById("story").style.display = "none";
+    document.getElementById("characters").style.display = "none";
+    document.getElementById("about").style.display = "none";
+
+    document.getElementById(id).style.display = "block";
 }
