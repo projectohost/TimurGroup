e500d496a46df3ee597bc7368a5fcd08c5641712
projectohost <serhiyduo@gmail.com>
2026-03-28 18:19:39 +0200

    changes fix

---
+++ main.js
@@ -0,0 +1,5 @@
+function showSection(id) {
+  document.getElementById("story").style.display = "none";
+  document.getElementById("characters").style.display = "none";
+  document.getElementById(id).style.display = "block";
+}
